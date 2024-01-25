f5bebc57994be50f195946081d30e94a10a23047
Vitalii Oleksenko <oanatol492@gmail.com>
2024-01-25 23:32:17 +0200

    add image compressor and multithreaded save

--- src/main/java/com/microservice/upload/service/FilesStorageServiceImpl.java
+++ src/main/java/com/microservice/upload/service/FilesStorageServiceImpl.java
@@ -6,23 +6,24 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Stream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.microservice.upload.model.FileInfo;
+import com.microservice.upload.util.ImageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
 @Service
 @Slf4j
 public class FilesStorageServiceImpl implements FilesStorageService{
@@ -30,6 +31,10 @@ public class FilesStorageServiceImpl implements FilesStorageService{
     @Value("${site.url}") String siteUrl;
     private final String uploadDir;
     private final Path root ;
+    private static final List<String> ALLOWED_IMAGE_TYPES = Arrays.asList("jpg", "jpeg", "png", "gif", "bmp");
+    private static final long MAX_IMAGE_SIZE = 2 * 1024 * 1024; // 2 MB
+    private final ExecutorService executorService = Executors.newFixedThreadPool(5);
+
 
     @Autowired
     public FilesStorageServiceImpl(@Value("${upload.dir}") String uploadDir ) {
@@ -51,28 +56,41 @@ public class FilesStorageServiceImpl implements FilesStorageService{
     }
     @Override
     public FileInfo save(MultipartFile file, String sessionId) throws IOException {
-        String newDir  = root.toString().concat("/").concat(sessionId);
+        String newDir = root.toString().concat("/").concat(sessionId);
         Path dir = Files.createDirectories(Path.of(newDir));
 
         String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
-        String uuid = UUID.randomUUID().toString().replace("-","");
+        String uuid = UUID.randomUUID().toString().replace("-", "");
         String extension = FilenameUtils.getExtension(originalFileName);
         String fileName = uuid + "." + extension.toLowerCase();
         String fileUrl = siteUrl.concat("/").concat(sessionId).concat("/").concat(fileName);
-        FileInfo fileInfo = new FileInfo(originalFileName,fileUrl);
-        try {
-            Files.copy(file.getInputStream(), dir.resolve(fileName));
-            log.info(String.format("File %s saved as %s, url: %s",originalFileName,dir.resolve(fileName).toString(),fileUrl));
-            return fileInfo;
-        } catch (Exception e) {
-            if (e instanceof FileAlreadyExistsException) {
-                log.error(String.format("A file of %s already exists",fileName));
-                throw new RuntimeException("A file of that name already exists.");
-            }
-            log.error(String.format("save %s error %s",originalFileName,e.getMessage()));
-            throw new RuntimeException(e.getMessage());
-        }
+        FileInfo fileInfo = new FileInfo(originalFileName, fileUrl);
 
+        if (isImageFile(extension) && file.getSize() > MAX_IMAGE_SIZE) {
+            executorService.submit(new ImageUtil(file, extension, dir.resolve(fileName)));
+            executorService.shutdown();
+            System.out.println(String.format("File %s saved as %s, url: %s", originalFileName, dir.resolve(fileName).toString(), fileUrl));
+
+            return fileInfo;
+        } else {
+            try {
+                Files.copy(file.getInputStream(), dir.resolve(fileName));
+                System.out.println(String.format("Non-image file %s saved as %s, url: %s", originalFileName, dir.resolve(fileName).toString(), fileUrl));
+
+                return fileInfo;
+            } catch (Exception e) {
+                if (e instanceof FileAlreadyExistsException) {
+                    log.error(String.format("A file of %s already exists",fileName));
+                    throw new RuntimeException("A file of that name already exists.");
+                }
+                log.error(String.format("save %s error %s",originalFileName,e.getMessage()));
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
+
+    private boolean isImageFile(String extension) {
+        return ALLOWED_IMAGE_TYPES.contains(extension.toLowerCase());
     }
 
     @Override
